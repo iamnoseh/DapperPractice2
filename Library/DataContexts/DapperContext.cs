73968ec4bdae534e2b50692f87227656072edabb
Noseh Taghaymurodzoda <noseh.tagaymurodzoda@gmail.com>
2024-12-09 18:14:23 +0500

    fix bug

--- Library/DataContexts/DapperContext.cs
+++ Library/DataContexts/DapperContext.cs
@@ -3,7 +3,7 @@ using Npgsql;
 
 public class DapperContext
 {
-    private readonly string connectionString = "Server = localhots;Database = companydb; User Id=postgres;Port=5432;Password=12345;";
+    private readonly string connectionString = "Server = localhots;Database = coursedb; User Id=postgres;Port=5432;Password=12345;";
 
     public NpgsqlConnection GetConnection()
     {
